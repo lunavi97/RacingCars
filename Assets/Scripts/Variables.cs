bf4b258876163577385170a39d3733165b39a9ce
Luciano Nahuel Vitale <lunavi97@gmail.com>
2018-09-17 17:51:45 -0300

    Creado script con variables y mostradas en consola

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour {
+
+    int numero = 10;
+    float unDecimal = 10.5f;
+    string texto = "Esto es un string";
+    bool booleana = true;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(numero);
+        Debug.Log(unDecimal);
+        Debug.Log(texto);
+        Debug.Log(booleana);
+	}
+}
